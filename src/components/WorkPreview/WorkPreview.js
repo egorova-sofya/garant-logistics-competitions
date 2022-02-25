c60534e57299a98f6044e0e5a05682c13404c823
egorova-sofya <egorovaSjob@yandex.ru>
2022-02-25 13:59:42 +0300

    fixed colors to likes component

--- src/components/WorkPreview/WorkPreview.js
+++ src/components/WorkPreview/WorkPreview.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import testImg from "../../assets/img/testImg.jpg";
+import LikesCounter from "../LikesCounter/LikesCounter";
 import s from "./WorkPreview.module.css";
 
-const WorkPreview = ({ img, title, link }) => {
+const WorkPreview = ({ img, title, link, active = false }) => {
   return (
     <>
-      <Link to="#">
+      <Link className={s.link} to="#">
         <div className={s.imgWrapper}>
           <div className={s.imgHover}></div>
           <img className={s.img} src={testImg} />
+        </div>
+        <div className={s.likesCounterWrapper}>
+          <LikesCounter active={active} />
         </div>
       </Link>
     </>
--- src/pages/WorksListPage/WorksListPage.js
+++ src/pages/WorksListPage/WorksListPage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import WorkPreview from "../../components/WorkPreview/WorkPreview";
 import { withLayout } from "../../hoc/Layout/Layout";
+import AddWorkBtn from "../../components/AddWorkBtn/AddWorkBtn";
 import s from "./WorksListPage.module.css";
 
 const WorksListPage = ({ title }) => {
@@ -8,6 +9,7 @@ const WorksListPage = ({ title }) => {
     <>
       <h1 className={s.title}>{title}</h1>
       <dix className={s.workPreviewWrapper}>
+        <AddWorkBtn />
         <WorkPreview />
         <WorkPreview />
         <WorkPreview />
