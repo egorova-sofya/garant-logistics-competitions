702734d4124ae0309d172df3e684be2aa164c795
egorova-sofya <egorovaSjob@yandex.ru>
2022-03-03 14:09:05 +0300

    extended dropzone

--- src/components/Dropzone/Dropzone.js
+++ src/components/Dropzone/Dropzone.js
@@ -1,45 +1,65 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { useDropzone } from "react-dropzone";
 import s from "./Dropzone.module.css";
+import fileImg from "../../assets/img/file.svg";
 
 const Dropzone = () => {
   const [files, setFiles] = useState([]);
 
-  const { getRootProps, getInputProps, open } = useDropzone({
-    accept: "image/*",
-    onDrop: (acceptedFiles) => {
-      setFiles(
-        acceptedFiles.map((file) =>
-          Object.assign(file, {
-            preview: URL.createObjectURL(file),
-          })
-        )
-      );
-    },
-  });
+  const onDrop = useCallback((acceptedFiles) => {
+    acceptedFiles.forEach((file) => {
+      const reader = new FileReader();
+
+      reader.onabort = () => console.log("file reading was aborted");
+      reader.onerror = () => console.log("file reading has failed");
+      reader.onload = () => {
+        // Do whatever you want with the file contents
+        const binaryStr = reader.result;
+        console.log(binaryStr);
+        setFiles(
+          acceptedFiles.map((file) =>
+            Object.assign(file, {
+              preview: URL.createObjectURL(file),
+            })
+          )
+        );
+      };
+      reader.readAsArrayBuffer(file);
+    });
+  }, []);
+
+  const { getRootProps, getInputProps, open } = useDropzone({ onDrop });
 
   const images = files.map((file) => (
     <div key={file.name}>
       <div>
-        <img src={file.preview} style={{ width: "200px" }} alt="preview" />
+        <img src={fileImg} style={{ width: "100px" }} alt="preview" />
+        <p className={s.fileName}>{file.name}</p>
       </div>
     </div>
   ));
 
   return (
     <>
-      <div className={s.modalWrapper}>
-        <div className={s.container}>
-          <div {...getRootProps()}>
+      <button
+        className={s.button}
+        type="button"
+        onClick={open}
+        {...getRootProps()}
+      >
+        <div className={s.modalWrapper}>
+          <div className={s.container}>
             <input {...getInputProps()} />
-            <p className={s.text}>Выберите файл или перенесите его сюда</p>
+            {images.length > 0 ? (
+              <></>
+            ) : (
+              <p className={s.text}>Выберите файл или перенесите его сюда</p>
+            )}
+
+            <div>{images}</div>
           </div>
-          <div>{images}</div>
         </div>
-        <button className={s.button} type="button" onClick={open}>
-          Open File Dialog
-        </button>
-      </div>
+      </button>
     </>
   );
 };
--- src/pages/AddWorkPage/AddWorkPage.js
+++ src/pages/AddWorkPage/AddWorkPage.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Dropzone from "../../components/Dropzone/Dropzone";
 import { withLayout } from "../../hoc/Layout/Layout";
+import s from "./AddWorkPage.module.css";
 
 const AddWorkPage = () => {
   return (
     <>
-      AddWorkPage
-      <Dropzone />
+      <div className={s.wrapper}>
+        <Dropzone />
+      </div>
     </>
   );
 };
